d02f6c11cdb6f09e7075a016ab26c1e3c846c32d
Derek Nguyen <derek@penandpillow.com>
2019-10-20 00:57:55 +0900

    (feat) Export a generic function that accepts path as argument (#1)
    * add generic function that accept inline arguments
    
    * update readme

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -6,15 +6,40 @@ import { PLUGIN_NAME } from './constants'
 import { createFieldExts } from './utils'
 
 type Resolve = (...args: any[]) => any
-type CreateResolve = ({ baseDir }: { baseDir: string }) => Resolve
+type ResolveOptions = Record<'path', string>
+interface CreateResolveArgs {
+  rootDir: string;
+  dir?: string;
+  options?: ResolveOptions;
+}
+type CreateResolve = (args: CreateResolveArgs) => Resolve
 const createResolve: CreateResolve = ({
-  baseDir,
-}) => (src: any, args: any, context: any, info: any) => {
+  options,
+  dir,
+  rootDir,
+}) => (src: any, _: any, context: any, info: any) => {
   const { fieldName } = info
   const partialPath = src[fieldName]
     if (!partialPath) {
       return null
     }
+
+  // if baseDir is not passed in, generate
+  // a generic resolve function that
+  // accepts options instead
+  let basePath
+  if (dir) {
+    basePath = dir
+  } else {
+    if (!options || typeof options.path !== 'string') {
+      return null
+    }
+    basePath = options.path
+  }
+  const baseDir = path.join(rootDir, basePath)
+  if (!fs.existsSync(baseDir)) {
+    throw new Error(`${PLUGIN_NAME}: ${basePath} doesn't exist`)
+  }
 
   const filePath = path.join(baseDir, partialPath)
   const fileNode = context.nodeModel.runQuery({
@@ -41,17 +66,13 @@ export const createSchemaCustomization: GatsbyNode['createSchemaCustomization'] 
   options: PluginOptions,
 ) => {
   const { dirs } = options
-  const progDir = store.getState().program.directory
+  const rootDir = store.getState().program.directory
 
   try {
     const fieldExts = createFieldExts(dirs)
 
     fieldExts.forEach(entry => {
       const { dir, name } = entry
-      const baseDir = path.join(progDir, dir)
-      if (!fs.existsSync(baseDir)) {
-        throw new Error(`${PLUGIN_NAME}: ${dir} doesn't exist`)
-      }
 
       if (process.env.NODE_ENV !== 'production') {
         reporter.info(`${PLUGIN_NAME}: Field extension created! Use @${name} for ${dir}`)
@@ -59,11 +80,29 @@ export const createSchemaCustomization: GatsbyNode['createSchemaCustomization'] 
 
       actions.createFieldExtension({
         name,
-        extend: () => ({
-          resolve: createResolve({ baseDir })
+        extend: (options: ResolveOptions) => ({
+          resolve: createResolve({ options, dir, rootDir })
         })
       })
     })
+
+    // generic function
+    actions.createFieldExtension({
+      name: 'fileByAbsolutePath',
+      args: {
+        path: {
+          type: 'String!',
+          defaultValue: '',
+        },
+      },
+      extend: (options: ResolveOptions) => ({
+        args: {
+          path: 'String',
+        },
+        resolve: createResolve({ options, rootDir })
+      })
+    })
+
   } catch(err) {
     reporter.warn(err)
   }
